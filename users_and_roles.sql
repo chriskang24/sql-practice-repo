c44ec6cd42d589eb2ebfb64d6c3976f7a7bcbd4c
chriskang24 <christopherjkang24@gmail.com>
2021-04-05 22:40:57 -0400

    users_and_roles.sql exercise complete

---
+++ users_and_roles.sql
@@ -0,0 +1,23 @@
+-- TABLE users
+--   id INTEGER NOT NULL PRIMARY KEY,
+--   userName VARCHAR(50) NOT NULL
+
+-- TABLE roles
+--   id INTEGER NOT NULL PRIMARY KEY,
+--   role VARCHAR(20) NOT NULL
+-- The users_roles table should contain the mapping between each user and their roles. Each user can have many roles, and each role can have many users.
+
+-- Modify the provided SQLite create table statement so that:
+
+-- Only users from the users table can exist within users_roles.
+-- Only roles from the roles table can exist within users_roles.
+-- A user can only have a specific role once.
+
+CREATE TABLE users_roles (
+  id INTEGER NOT NULL PRIMARY KEY,
+  userId INTEGER NOT NULL,
+  roleId INTEGER NOT NULL,
+  FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (roleId) REFERENCES roles(id) ON DELETE CASCADE,
+  UNIQUE (userId, roleId)
+);
